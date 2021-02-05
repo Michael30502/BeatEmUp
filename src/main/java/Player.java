77e80e65894a8824386bde7c05800747487198a4
Michael Bendtsen <mich.bendtsen@gmail.com>
2021-02-05 10:21:46 +0100

    Movement/ combat

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -8,7 +8,13 @@ public class Player {
 
 PVector position = new PVector();
 PVector velocity = new PVector();
+    PVector attackZonePos = new PVector(50,50);
+
 PApplet p;
+int playerWidth = 10;
+int playerHeight=10;
+boolean down,up,left,right = false;
+boolean ready = true;
 
 Player(PApplet p){
     this.p = p;
@@ -19,36 +25,90 @@ position.set(100,this.p.height/2);
 
 void changePosition(){
 
-
+position.add(velocity);
+p.println(p.width);
+    position.x =p.constrain(position.x,0,p.width-playerWidth);
+    position.y=  p.constrain(position.y,0,p.height-playerHeight);
 }
 
 
     void draw(){
-
-        p.rect(position.x,position.y,10,10);
         changePosition();
+        p.rect(position.x,position.y,playerWidth,playerHeight);
+        changePosition();
+       // if(AttackZone)
+       // p.rect(position.x+playerWidth,position.y+playerHeight,position.x+attackZonePos.x,position.y,attackZonePos.y);
     }
 
+    void attack(int attackType){
+    /*1 = punch
+
+
+
+    */
+
+
+    if(attackType== 1){
+       // createAttackZone();
+
+    }
+
+
+
+    }
+
+/*
+createAttackZone(){
+
+    //attackZones = true;
+
+
+}
+*/
+
+
+
+
 void controls(char key, int keyCode,  boolean pressed){
+   velocity.set(0,0);
     if (key != p.CODED)
     switch(key){
 
+        case 'f':{
+            if(ready) {
+                ready = false;
+                attack(1);
+
+            }
+        }
 
         case 's': {
-            velocity.add(0,-1);
+            if((pressed) &&(ready))
+            down=true;
+            else{
+                down=false;}
 
         }break;
 
         case 'w': {
-            velocity.add(0,1);
+            if((pressed) &&(ready))
+            up = true;
+            else
+                up=false;
 
         }break;
         case 'a': {
-            velocity.add(1,0);
+            if((pressed) &&(ready))
+            left=true;
+            else
+                left=false;
 
         }break;
         case 'd': {
-            velocity.add(-1,0);
+            if((pressed) &&(ready))
+            right=true;
+            else
+                right=false;
 
         }break;
 
@@ -58,20 +118,34 @@ else{
     switch (keyCode){
 
         case DOWN: {
-            velocity.add(0,-1);
+            if((pressed) &&(ready))
+            down=true;
+            else{
+                down=false;}
 
         }break;
 
         case UP: {
-            velocity.add(0,1);
+            if((pressed) &&(ready))
+                up=true;
+            else{
+                up=false;}
 
         }break;
         case RIGHT: {
-            velocity.add(1,0);
+            if((pressed) &&(ready))
+                right=true;
+            else{
+                right=false;}
+
 
         }break;
         case LEFT: {
-            velocity.add(-1,0);
+            if((pressed) &&(ready))
+                left=true;
+            else{
+                left=false;}
+
 
         }break;
 
@@ -79,7 +153,7 @@ else{
 
 
     }
-
+    velocity.set(((right)?1:0) +((left)?-1:0),(((up)?-1:0) +((down)?1:0)));
 }
 
 
