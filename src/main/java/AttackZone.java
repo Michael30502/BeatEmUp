20ac2d43783aaea26dd2e7002ee653b015dc3e8e
Michael Bendtsen <mich.bendtsen@gmail.com>
2021-04-10 13:42:42 +0200

    AttackZoneFix

--- src/main/java/AttackZone.java
+++ src/main/java/AttackZone.java
@@ -3,12 +3,18 @@ import processing.core.PVector;
 
 public class AttackZone {
 
-    int attackType;
+
     PApplet p;
     PVector position;
+    PVector zonePosition = new PVector();
+
+    int attackType;
     int playerWidth;
     int playerHeight;
     int scale;
+    int zoneWidth = 50;
+    int zoneHeight = 50;
+
 
     AttackZone(int attackType, PApplet p, PVector position,int playerWidth,int playerHeight,int scale ){
     this.attackType = attackType;
@@ -16,7 +22,9 @@ public class AttackZone {
     this.position = position;
     this.playerWidth = playerWidth;
     this.playerHeight = playerHeight;
-
+    this.scale = scale;
+    zonePosition.set(position.x+playerWidth*scale,position.y+(playerHeight/5));
+    zoneWidth *= scale;
     }
 
     void createAttackZone(){
@@ -26,10 +34,12 @@ public class AttackZone {
     }
 
     void displayAttackZone(boolean damage){
+
+
        if(damage)
         p.fill(255,0,0);
         p.rectMode(0);
-        p.rect(position.x+playerWidth,position.y+(playerHeight/5),50,50);
+        p.rect(zonePosition.x, zonePosition.y,zoneWidth,zoneHeight);
         p.fill(255,255,255);
     }
 
